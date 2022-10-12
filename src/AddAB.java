903e979583517ca62436cc6c4fa76859e7002b36
yongbeom76 <yongbeom76@gmail.com>
2022-10-12 13:40:15 +0900

    addab

---
+++ src/AddAB.java
@@ -0,0 +1,17 @@
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+import java.util.Scanner;
+
+public class AddAB {
+    public static void main(String[] args){
+// 입력
+// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+// 출력
+// 첫째 줄에 A+B를 출력한다.    
+        System.out.println(A+B);
+        in.close();
+    }
+}
